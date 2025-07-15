5051650145a50cf78251ee52a6033db275f1c5dd
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-07-15 22:56:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 const speech = new SpeechSynthesisUtterance();
 const voiceList = document.querySelector("select");
 const speakBtn = document.querySelector("button");
+function loadVoices() {
+  const voices = window.speechSynthesis.getVoices();
+  voiceList.innerHTML = "";
+  voices.forEach((voice, index) => {
+    const option = document.createElement("option");
+    option.value = index;
+    option.text = `${voice.name} (${voice.lang})`;
+    voiceList.appendChild(option);
+  });
